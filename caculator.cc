8aa2c87172649a25b9f7ae14f71d0b986598dd1e
cn-zxcv <1032980965@qq.com>
2015-04-10 05:03:56 -0700

    simple

--- caculator.cc
+++ caculator.cc
@@ -1,2 +1,28 @@
 // simple caculator
 
+#include <iostream>
+
+using namespace std;
+
+float get_result(int a, int b, char op)
+{
+	switch(op)
+	{
+		case '-':
+			return a - b;
+		case '+':
+			return a + b;
+	}
+}
+
+int main()
+{
+	cout << "input a,b and op" << endl;
+	int a = 0, b = 0;
+	char op;
+	float result = 0.0;
+	
+	cin >> a >> b >> op;
+
+	cout << get_result(a, b, op) << endl;
+}
